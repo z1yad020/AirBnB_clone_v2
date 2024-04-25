c9246c4a4e5ae7607aa863ab34bebab7c2bc44e7
Ziyad Ibrahim <ziyad.ibrahim.112358@gmail.com>
2024-04-25 20:23:35 +0400

    pep8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,7 +6,8 @@ what am i doing
 
 from api.v1.views import app_views
 
+
 @app_views.route("/status")
 def status():
     """show api status"""
-    return {"status":"OK"}
+    return {"status": "OK"}
