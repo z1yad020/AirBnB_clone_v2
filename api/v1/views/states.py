4661876b0f2c373be11c5e27183edb4668532fcd
aelitaa <gunel.sultanova@outlook.com>
2024-04-26 00:07:19 +0400

    silent=True

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -44,7 +44,7 @@ def delete_state(state_id):
 @app_views.route('/states/', methods=['POST'], strict_slashes=False)
 def post_state():
     """create a new state"""
-    if not request.get_json():
+    if not request.get_json(silent=True):
         return make_response(jsonify({'error': 'Not a JSON'}), 400)
     if 'name' not in request.get_json():
         return make_response(jsonify({'error': 'Missing name'}), 400)
