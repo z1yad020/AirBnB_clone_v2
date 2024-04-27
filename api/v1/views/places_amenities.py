f9a88c3d255819bc37cb7530ee60829db622c929
Ziyad Ibrahim <ziyad.ibrahim.112358@gmail.com>
2024-04-27 16:54:01 +0400

    checking get method

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""View for the link between Place objects and Amenity objects that handles all default RESTFul API actions"""
+"""
+View for the link between Place objects and Amenity objects
+that handles all default RESTFul API actions
+"""
 
 
 from api.v1.views import app_views
@@ -9,6 +12,7 @@ from flask import jsonify, request, abort
 from models.place import Place
 from models.user import User
 from models.amenity import Amenity
+from os import getenv
 
 
 @app_views.route("places/<place_id>/amenities", strict_slashes=False)
@@ -18,4 +22,13 @@ def amenity_of_places(place_id):
     if place is None:
         abort(404, "Not found")
 
-    return [am.to_dict() for am in place.amenities]
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        return [am.to_dict() for am in place.amenities]
+    return [am.to_dict() for am in place.amenity_ids]
+
+
+@app_views.route("places/<place_id>/amenities/<amenity_id>",
+                 methods=['DELETE', 'POST'],
+                 strict_slashes=False)
+def del_post_amenity(pl):
+    pass
